c731c4205002e466fc501ee791b92fee902ba242
Gregers Rørdam <gregers@rordam.dk>
2021-09-08 21:30:28 +0200

    Implement user input using TDD

--- Assignment0.Tests/ProgramTests.cs
+++ Assignment0.Tests/ProgramTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using Xunit;
 
 namespace Assignment0.Tests
@@ -13,9 +15,7 @@ namespace Assignment0.Tests
         [InlineData(1992)]
         public void IsLeapYear_DivisibleBy4ButNot100IsTrue(int year)
         {
-            Assert.True(
-                Assignment0.Program.IsLeapYear(year)
-            );
+            Assert.True(Assignment0.Program.IsLeapYear(year));
         }
 
         [Theory()]
@@ -27,9 +27,7 @@ namespace Assignment0.Tests
         [InlineData(2002)]
         public void IsLeapYear_NotDivisibleBy4IsFalse(int year)
         {
-            Assert.False(
-                Assignment0.Program.IsLeapYear(year)
-            );
+            Assert.False(Assignment0.Program.IsLeapYear(year));
         }
 
         [Theory()]
@@ -39,9 +37,7 @@ namespace Assignment0.Tests
         [InlineData(3000)]
         public void IsLeapYear_DivisibleBy4And100ButNot400IsFalse(int year)
         {
-            Assert.False(
-                Assignment0.Program.IsLeapYear(year)
-            );
+            Assert.False(Assignment0.Program.IsLeapYear(year));
         }
 
 
@@ -53,9 +49,50 @@ namespace Assignment0.Tests
         [InlineData(2800)]
         public void IsLeapYear_DivisibleBy400IsTrue(int year)
         {
-            Assert.True(
-                Assignment0.Program.IsLeapYear(year)
-            );
+            Assert.True(Assignment0.Program.IsLeapYear(year));
+        }
+
+        [Fact]
+        public void IsLeapYear_PrintsPrompt()
+        {
+            var stdout = new StringWriter();
+            var stdin = new StringReader("0\n");
+            Console.SetOut(stdout);
+            Console.SetIn(stdin);
+            Program.Main(new string[0]);
+            var firstLine = stdout.ToString().Split("\n")[0].Trim();
+            Assert.Equal("Please type in a year to find out whether it's a leap year. Then hit [enter]:", 
+                        firstLine);
+
+        }
+
+        [Fact]
+        public void IsLeapYear_PrintsYayOnTrue()
+        {
+            var stdout = new StringWriter();
+            var stdin = new StringReader("2000\n");
+            Console.SetOut(stdout);
+            Console.SetIn(stdin);
+            Program.Main(new string[0]);
+            var lastLine = stdout.ToString().Split("\n")[1].Trim();
+            Assert.Equal("yay", 
+                        lastLine);
+
+        }
+
+
+        [Fact]
+        public void IsLeapYear_PrintsNayOnFalse()
+        {
+            var stdout = new StringWriter();
+            var stdin = new StringReader("1997\n");
+            Console.SetOut(stdout);
+            Console.SetIn(stdin);
+            Program.Main(new string[0]);
+            var lastLine = stdout.ToString().Split("\n")[1].Trim();
+            Assert.Equal("nay", 
+                        lastLine);
+
         }
     }
 }
